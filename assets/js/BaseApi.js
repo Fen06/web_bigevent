cb0fb2de6225b7e5200f017231449d1feb73381c
18God <1226343906@qq.com>
2022-06-11 09:49:02 +0800

    提交index

--- assets/js/BaseApi.js
+++ assets/js/BaseApi.js
@@ -1,7 +1,27 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter((option) => {
+$.ajaxPrefilter((options) => {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  option.url = `http://www.liulongbin.top:3007` + option.url;
+  options.url = `http://www.liulongbin.top:3007` + options.url;
+
+  if (options.url.includes('/my/')) {
+    options.headers = {
+      Authorization: localStorage.getItem('token'),
+    };
+  }
+
+  //权限校验
+  options.complete = (res) => {
+    // console.log(res);
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === '身份认证失败！'
+    ) {
+      // 1.清楚本地存储的token
+      localStorage.removeItem('token');
+      // 2.跳转到登录页面
+      location.href = '/login.html';
+    }
+  };
 });
